47b2ddb627e10890582c1b10df88d397b95a45d4
Radim Staněk <stanek@gtomy.com>
2023-01-20 23:52:27 +0100

    Prettier setup fix pipelines

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,5 @@
-import React from "react";
+import React from 'react';
 
 export default function Button() {
-    return <button type="button">Super button</button>
+  return <button type="button">Super button</button>;
 }
