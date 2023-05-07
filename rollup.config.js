cbc99d6d5e86a5366f487a45d1ad158d1a6d1757
Radim Staněk <stanek@gtomy.com>
2023-05-07 22:17:41 +0200

    chore: fix build

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
+import json from '@rollup/plugin-json';
 import dts from 'rollup-plugin-dts';
 
 const packageJson = require('./package.json');
@@ -20,6 +21,7 @@ export default [
       peerDepsExternal(),
       resolve(),
       commonjs(),
+      json(),
       typescript({ tsconfig: './tsconfig.json' }),
     ],
     external: ["react", "react-dom", "react-router-dom", "react-hook-form", "theme-change"],
