fa852149f63af87ff4a47c44e46e6a5aa49b932e
Radim Staněk <stanek@gtomy.com>
2023-08-18 06:21:32 +0000

    feat(components): add dialog and useDialog hook for easy usage

---
+++ src/utils/hooks/storage/useDialogStore.ts
@@ -0,0 +1,24 @@
+import { create } from 'zustand';
+
+export interface DialogObject {
+  id: string;
+  open: boolean;
+  element: JSX.Element;
+}
+
+export interface DialogStore {
+  dialogs: DialogObject[];
+  setDialogs: (dialogs: JSX.Element[]) => void;
+  addDialog: (dialog: JSX.Element) => void;
+  setOpened: (id: string, open: boolean) => void;
+}
+
+export const useDialogStore = create((set) => ({
+  dialogs: [],
+  setDialogs: (dialogs: DialogObject[]) => set(() => ({ dialogs: dialogs })),
+  addDialog: (dialog: DialogObject) => set((store: DialogStore) => ({ dialogs: [...store.dialogs, dialog] })),
+  setOpened: (id: string, open: boolean) =>
+    set((store: DialogStore) => ({
+      dialogs: store.dialogs.map((dialog) => (dialog.id === id ? { ...dialog, open: open } : dialog)),
+    })),
+}));
