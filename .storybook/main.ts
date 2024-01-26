2de79c2b200661fb876127ea9c8937b4981fb55b
GTomy <stanek@gtomy.net>
2024-01-26 21:12:44 +0100

    docs: storybook documentation

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,11 +2,12 @@ import type { StorybookConfig } from '@storybook/react-webpack5';
 import path from 'path';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-mdx-gfm',
     {
       name: '@storybook/addon-styling',
       options: {
@@ -19,7 +20,8 @@ const config: StorybookConfig = {
     options: {},
   },
   docs: {
-    autodocs: 'tag',
+    autodocs: true,
+    defaultName: 'Documentation',
   },
   webpackFinal: async (config) => {
     if (!config.resolve) {
