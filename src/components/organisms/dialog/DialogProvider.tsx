8048ce4a4e8b91088d054ed4b3ab39cfaaff7865
GTomy <stanek@gtomy.net>
2024-02-01 18:18:26 +0100

    fix: dialog not opening

--- src/components/organisms/dialog/DialogProvider.tsx
+++ src/components/organisms/dialog/DialogProvider.tsx
@@ -1,5 +1,6 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useMemo } from 'react';
 import { useDialog } from '@/utils/hooks/useDialog';
+import { DialogObject } from '@/utils';
 
 export interface DialogProviderProps {
   children: ReactNode;
@@ -7,6 +8,19 @@ export interface DialogProviderProps {
 
 export function DialogProvider({ children }: DialogProviderProps) {
   const { dialogs, openDialog, closeDialog } = useDialog();
+
+  const flattenedDialog = useMemo(() => {
+    const ret: DialogObject[] = [];
+    for (const dialog of dialogs) {
+      const dialogIndex = ret.findIndex((d) => d.id === dialog.id);
+      if (dialogIndex === -1) {
+        ret.push(dialog);
+      } else {
+        ret[dialogIndex] = dialog;
+      }
+    }
+    return ret;
+  }, [dialogs]);
 
   const onOpenChange = (id: string, open: boolean) => {
     if (open) {
@@ -18,7 +32,7 @@ export function DialogProvider({ children }: DialogProviderProps) {
 
   return (
     <>
-      {dialogs.map((dialog) =>
+      {flattenedDialog.map((dialog) =>
         React.cloneElement(dialog.element, {
           key: dialog.id,
           id: dialog.id,
