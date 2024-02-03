dc5130255fc8dc211de4c2104f7942b1700c9c11
GTomy <stanek@gtomy.net>
2024-02-03 11:11:05 +0100

    feat: add icons for social media

--- src/components/atoms/Footer/FooterSocialItem.tsx
+++ src/components/atoms/Footer/FooterSocialItem.tsx
@@ -1,5 +1,18 @@
 import React, { ComponentProps } from 'react';
+import { Threads } from '@/assets/social/Threads';
+import { Youtube } from '@/assets/social/Youtube';
+import { Instagram } from '@/assets/social/Instagram';
 
-export function FooterSocialItem({ children, ...other }: ComponentProps<'a'>) {
-  return <a {...other}>{children}</a>;
+const types = {
+  threads: <Threads />,
+  youtube: <Youtube />,
+  instagram: <Instagram />,
+};
+
+export interface FooterSocialItemProps extends Omit<ComponentProps<'a'>, 'children'> {
+  type: 'threads' | 'youtube' | 'instagram';
+}
+
+export function FooterSocialItem({ type, ...other }: FooterSocialItemProps) {
+  return <a {...other}>{types[type]}</a>;
 }
