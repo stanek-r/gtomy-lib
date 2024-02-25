b8d4c23099f3246f73783a754e97612bbc101f00
GTomy <stanek@gtomy.net>
2024-02-25 12:24:35 +0100

    fix: withColumnPage width

--- src/utils/withHelpers.tsx
+++ src/utils/withHelpers.tsx
@@ -9,17 +9,17 @@ import { getUsersRoleId } from '@/utils/auth';
  * Renders a page with a column layout, consisting of a main component and optional menu and footer components.
  *
  * @param {FunctionComponent | JSX.Element} Component - The main component to render within the page.
+ * @param {'sm' | 'md' | 'lg' | 'xl' | '2xl'} [width] - The optional width of the page layout.
  * @param {FunctionComponent | JSX.Element} [MenuComponent] - The optional menu that will replace provided Menu component.
  * @param {FunctionComponent | JSX.Element} [FooterComponent] - The optional footer that will replace provided Footer component.
- * @param {'sm' | 'md' | 'lg' | 'xl' | '2xl'} [width] - The optional width of the page layout.
  *
  * @returns {JSX.Element} The rendered column page.
  */
 export function withColumnPage(
   Component: FunctionComponent | JSX.Element,
+  width?: 'sm' | 'md' | 'lg' | 'xl' | '2xl',
   MenuComponent?: FunctionComponent | JSX.Element,
-  FooterComponent?: FunctionComponent | JSX.Element,
-  width?: 'sm' | 'md' | 'lg' | 'xl' | '2xl'
+  FooterComponent?: FunctionComponent | JSX.Element
 ): JSX.Element {
   return (
     <ColumnPage MenuComponent={MenuComponent} FooterComponent={FooterComponent} width={width}>
