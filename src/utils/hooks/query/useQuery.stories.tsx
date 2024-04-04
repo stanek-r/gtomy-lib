c057f4f70c914d6284ea5f608eddfc96d55ded4f
GTomy <stanek@gtomy.net>
2024-04-04 19:11:12 +0200

    feat: add usePageQuery

--- src/utils/hooks/query/useQuery.stories.tsx
+++ src/utils/hooks/query/useQuery.stories.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { useQuery } from './useQuery';
 import { QueryClient } from '@tanstack/react-query';
 import { QueryWrapper } from '@/utils/hooks/query/QueryWrapper';
+import { Typography } from '@/components/atoms/Typography';
+import { Button } from '@/components/atoms/Button';
+import { PageQueryData, usePageQuery } from '@/utils/hooks/query/usePageQuery';
 
 export default {
   title: 'Hooks/useQuery',
@@ -19,18 +22,54 @@ const testCallFunction = async () => {
   await new Promise((resolve) => setTimeout(resolve, 2000));
   return 'test';
 };
+const testCallFunction2 = async (page: number, results: number) => {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  return { isLast: page >= 10, numberOfPages: 10, data: 'test ' + page + ' ' + results };
+};
 
 const queryClient = new QueryClient();
 
 export const Default = () => {
   const { wrapperProps, data } = useQuery<string | null>(
-    { fallbackValue: null, queryKey: ['test'], queryFn: () => testCallFunction(), showRetry: true },
+    { fallbackValue: null, queryKey: ['test'], queryFn: testCallFunction, showRetry: true },
     queryClient
   );
 
   return (
     <QueryWrapper {...wrapperProps}>
       <>{data}</>
+    </QueryWrapper>
+  );
+};
+
+interface StoryPage extends PageQueryData {
+  data: string | null;
+}
+
+export const Page = () => {
+  const { wrapperProps, data, page, nextPage, prevPage, numberOfPages } = usePageQuery<StoryPage>(
+    {
+      fallbackValue: { isLast: false, numberOfPages: 0, data: null },
+      queryKey: ['test'],
+      queryFn: testCallFunction2,
+      showRetry: true,
+      resultsPerPage: 10,
+    },
+    queryClient
+  );
+
+  return (
+    <QueryWrapper {...wrapperProps}>
+      <>
+        <Typography>{data.data}</Typography>
+        <div className="flex items-center justify-between gap-2">
+          <Button onClick={prevPage}>Prev</Button>
+          <Button>
+            {page}/{numberOfPages}
+          </Button>
+          <Button onClick={nextPage}>Next</Button>
+        </div>
+      </>
     </QueryWrapper>
   );
 };
