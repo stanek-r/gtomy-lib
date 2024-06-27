2a4a7ae26a0b7e76ca57e4a2823a89c7fbcc7af7
GTomy <stanek@gtomy.net>
2024-06-27 18:33:22 +0200

    feat!: update build to work with typescript 5

--- rollup.config.js
+++ rollup.config.js
@@ -5,23 +5,36 @@ import typescript from '@rollup/plugin-typescript';
 import alias from '@rollup/plugin-alias';
 import json from '@rollup/plugin-json';
 import css from 'rollup-plugin-import-css';
+import copy from 'rollup-plugin-copy';
+import del from 'rollup-plugin-delete';
 import { dts } from 'rollup-plugin-dts';
 import path from 'path';
-
-const packageJson = require('./package.json');
 
 export default [
   {
     input: 'src/index.ts',
     output: [
       {
-        file: packageJson.main,
+        file: 'dist/index.js',
         format: 'esm',
         sourcemap: true,
         inlineDynamicImports: true,
       },
     ],
-    plugins: [peerDepsExternal(), css(), resolve(), commonjs(), json(), typescript({ tsconfig: './tsconfig.json' })],
+    plugins: [
+      peerDepsExternal(),
+      css(),
+      resolve(),
+      commonjs(),
+      json(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      copy({
+        targets: [
+          { src: './package.json', dest: 'dist' },
+          { src: './README.md', dest: 'dist' },
+        ],
+      }),
+    ],
     external: [
       '@cloudflare/stream-react',
       '@radix-ui/react-dialog',
@@ -51,6 +64,10 @@ export default [
         ],
       }),
       dts(),
+      del({
+        targets: 'dist/types',
+        hook: 'buildEnd',
+      }),
     ],
   },
 ];
