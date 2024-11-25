12bc31ed1ac762ca7ca10fec99beb60a10089788
Radim Staněk <gtomyasek@gmail.com>
2024-11-25 21:38:36 +0100

    feat: redesign not found page

--- src/components/pages/NotFoundPage.tsx
+++ src/components/pages/NotFoundPage.tsx
@@ -1,27 +1,24 @@
-import { FormPage } from '@/components/layout';
 import { Typography } from '@/components/atoms/Typography';
 import { useTranslation } from '@/utils/hooks';
 import { Button } from '@/components/atoms/Button';
 import { Link } from 'react-router-dom';
+import { ColumnPage } from '@/components/layout';
 
 export function NotFoundPage() {
   const { t } = useTranslation('common');
 
   return (
-    <FormPage>
-      <div className="flex w-full flex-1 items-center justify-center text-center">
-        <div className="flex w-[768px] max-w-full flex-col gap-y-3 p-4">
-          <Typography size="3xl" weight="bold">
-            {t('notFound.title')}
-          </Typography>
-          <Typography size="3xl">{t('notFound.subtitle')}</Typography>
-          <div>
-            <Button as={Link} to="/" size="sm" outline>
-              {t('back')}
-            </Button>
-          </div>
+    <ColumnPage>
+      <div className="flex w-full flex-1 flex-col items-center justify-center gap-8">
+        <Typography size="3xl" weight="bold">
+          {t('notFound')}
+        </Typography>
+        <div>
+          <Button as={Link} to="/" wide outline>
+            {t('back')}
+          </Button>
         </div>
       </div>
-    </FormPage>
+    </ColumnPage>
   );
 }
