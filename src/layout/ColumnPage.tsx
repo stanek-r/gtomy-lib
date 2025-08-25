97b13b99da4cc2189608cfe2e2150c89f6c47b41
Radim Staněk <gtomyasek@gmail.com>
2025-08-25 18:21:53 +0200

    fix: ensure consistent layout structure by adding parent div with flex and min-height styles

--- src/layout/ColumnPage.tsx
+++ src/layout/ColumnPage.tsx
@@ -6,12 +6,13 @@ export function ColumnPage({ MenuComponent, FooterComponent, children, width = '
   const widthClass = columnPageWidthClasses[width];
 
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <WithComponent Component={MenuComponent} />
       <div className={twMerge('max-w-full mx-auto flex flex-col gap-4 px-4 py-8 flex-1', widthClass, className)}>
         {children}
+        <div className="h-20"></div>
       </div>
       <WithComponent Component={FooterComponent} />
-    </>
+    </div>
   );
 }
