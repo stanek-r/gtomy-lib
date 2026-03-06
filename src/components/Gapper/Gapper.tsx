1c74b686cb9bc3a60a6ba1b2b1c9b28bdaa335a1
Radim Staněk <gtomyasek@gmail.com>
2026-03-06 23:28:54 +0100

    refactor: memoize Gapper component and improve prop typing

--- src/components/Gapper/Gapper.tsx
+++ src/components/Gapper/Gapper.tsx
@@ -1,7 +1,28 @@
-import { GapperProps, gapperSizeClasses } from '@/components/Gapper/Gapper.core';
 import { twMerge } from 'tailwind-merge';
+import { memo, PropsWithChildren } from 'react';
 
-export function Gapper({ horizontal, size = 'md', children, className }: GapperProps) {
-  const classes = twMerge('flex', horizontal ? 'flex-row' : 'flex-col', gapperSizeClasses[size], className);
-  return <div className={classes}>{children}</div>;
+const gapperSizeClasses = {
+  1: 'gap-1',
+  2: 'gap-2',
+  4: 'gap-4',
+  6: 'gap-6',
+  8: 'gap-8',
+  16: 'gap-16',
+  32: 'gap-32',
+};
+
+export type GapperSize = keyof typeof gapperSizeClasses;
+
+export interface GapperProps extends PropsWithChildren {
+  size?: GapperSize;
+  horizontal?: boolean;
+  className?: string;
 }
+
+export const Gapper = memo(({ horizontal, size = 4, children, className }: GapperProps) => {
+  return (
+    <div className={twMerge('flex', horizontal ? 'flex-row' : 'flex-col', gapperSizeClasses[size], className)}>
+      {children}
+    </div>
+  );
+});
